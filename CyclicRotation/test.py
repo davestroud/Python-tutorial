80270e4ddcae221045ec81adaa35fa89b9760118
davestroud <david@davidstroud.me>
2019-12-18 14:20:33 -0600

    add other files

---
+++ CyclicRotation/test.py
@@ -0,0 +1,10 @@
+# import random
+
+# SUITS = ['Clubs', 'Diamonds', 'Hearts', 'Spades']
+# RANKS = ['2', '3', '4', '5', '6', '7', '8', '9', '10',
+#          'Jack', 'Queen', 'King', 'Ace']
+
+# rank = random.randrange(0, len(RANKS))
+# suit = random.randrange(0, len(SUITS))
+
+
